3cc7248a3726d19be2b3774e3b7314b9a43064cf
layanabsaro <layan.14.2003@gmail.com>
2024-12-30 20:49:01 +0200

    Add functionality to search for cars based on license plate number from the Data.gov API and design frontend UI with form and cards display

--- index.js
+++ index.js
@@ -5,41 +5,30 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", async (req, res) => {
-  try {
-    const response = await axios.get("https://bored-api.appbrewery.com/random");
-    const result = response.data;
-    console.log(result);
-    res.render("index.ejs", { data: result });
-  } catch (error) {
-    console.error("Failed to make request:", error.message);
-    res.render("index.ejs", {
-      error: error.message,
-    });
-  }
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
 app.post("/", async (req, res) => {
   try {
-    console.log(req.body);
-    const type = req.body.type;
-    const participants = req.body.participants;
-    const response = await axios.get(
-      `https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`
-    );
+    const vehicleNumber = req.body.vehicleNumber;
+
+    const response = await axios.get(`https://data.gov.il/api/3/action/datastore_search?resource_id=56063a99-8a3e-4ff4-912e-5966c0279bad&q=${vehicleNumber}`);
     const result = response.data;
-    console.log(result);
-    res.render("index.ejs", {
-      data: result[Math.floor(Math.random() * result.length)],
-    });
+
+    if (result.result.records.length > 0) {
+      res.render("index.ejs", { data: result.result.records[0] });
+    } else {
+      res.render("index.ejs", { error: "No data found for this vehicle number" });
+    }
   } catch (error) {
     console.error("Failed to make request:", error.message);
-    res.render("index.ejs", {
-      error: "No activities that match your criteria.",
-    });
+    res.render("index.ejs", { error: "There was an error retrieving the data." });
   }
 });
 
