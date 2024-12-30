b5a7702ccca4a78b6d66b3677240eb799f279590
layanabsaro <layan.14.2003@gmail.com>
2024-12-30 21:36:52 +0200

    Updated index.js, index.ejs, and added public/styles/main.css

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import axiosRetry from "axios-retry";
 
 const app = express();
 const port = 3000;
 
-
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// إعداد مكتبة axios-retry لإعادة المحاولة
+axiosRetry(axios, {
+  retries: 3,  // عدد المحاولات لإعادة الاتصال
+  retryDelay: axiosRetry.exponentialDelay,  // تأخير عشوائي بين المحاولات
+  retryCondition: (error) => error.response.status === 429,  // إعادة المحاولة عند رمز الحالة 429 فقط
+});
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get("https://data.gov.il/api/3/action/datastore_search?resource_id=c8b9f9c8-4612-4068-934f-d4acd2e3c06e&limit=5");
+    const result = response.data.result.records;
+    console.log(result);
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
 app.post("/", async (req, res) => {
   try {
+    console.log(req.body);
     const vehicleNumber = req.body.vehicleNumber;
+    
+    const response = await axios.get(
+      `https://data.gov.il/api/3/action/datastore_search?resource_id=c8b9f9c8-4612-4068-934f-d4acd2e3c06e&q=${vehicleNumber}`
+    );
+    const result = response.data.result.records;
 
-    const response = await axios.get(`https://data.gov.il/api/3/action/datastore_search?resource_id=56063a99-8a3e-4ff4-912e-5966c0279bad&q=${vehicleNumber}`);
-    const result = response.data;
-
-    if (result.result.records.length > 0) {
-      res.render("index.ejs", { data: result.result.records[0] });
+    if (result.length > 0) {
+      res.render("index.ejs", { data: result[0] });
     } else {
       res.render("index.ejs", { error: "No data found for this vehicle number" });
     }
   } catch (error) {
     console.error("Failed to make request:", error.message);
-    res.render("index.ejs", { error: "There was an error retrieving the data." });
+    res.render("index.ejs", {
+      error: "There was an error retrieving the data.",
+    });
   }
 });
 
